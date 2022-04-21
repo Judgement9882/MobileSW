7439dc571f6f6107825e437fc0e50c8621af74de
Judgement9882 <79979086+Judgement9882@users.noreply.github.com>
2022-04-21 15:33:08 +0900

    220421
    220421

---
+++ 8주차 중간고사/4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleAppMiddleTest
+{
+    class UNIV
+    {
+        private int a;
+        public static int b;
+        public static int c;
+        public int res;
+
+        // 전체 인스턴스 개수를 카운트하는 변수
+        public static int count_UNIV=0;
+
+        public UNIV (int a)
+        {
+            this.a = a;
+            Console.WriteLine(a * (b + c));
+            res = a * (b + c);
+
+            // 생성될 때 마다 인스턴스를 카운트 합니다.
+            count_UNIV++;
+        }
+
+        public static void Sum(int m, int n)
+        {
+            Console.WriteLine(m + n);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // b와 c는 임의로 바꿉니다.
+            int b = 5;
+            int c = 15;
+
+            UNIV.b = b;
+            UNIV.c = c;
+            
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            UNIV.Sum(b, c);
+
+            UNIV univ_a = new UNIV(3);
+            UNIV univ_b = new UNIV(4);
+            // 인스턴스를 여러개 생성하여도 카운트 됩니다.
+            //UNIV univ_c = new UNIV(4);
+            //UNIV univ_d = new UNIV(5);
+            //UNIV univ_e = new UNIV(6);
+
+            if (univ_a.res.Equals(univ_b.res))
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+            
+            // 전체 인스턴스 생성 개수를 출력합니다.
+            Console.WriteLine(UNIV.count_UNIV);
+        }
+    }
+}
---
+++ 8주차 중간고사/5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleAppMiddleTest
+{
+    class GUGU
+    {
+        public static int n;
+        public static int m;
+        private int G;
+
+        public GUGU (int num)
+        {
+            // 생성시 정수 G값을 만든다.
+            G = num;
+
+            for (int i = 1; i<=n; i++)
+            {
+                Console.Write("{0}*{1} = {2,2}      ", i, G, i * G);
+
+                // 한 줄에는 m개 씩 출력됩니다.
+                if (i % m == 0) Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // n과 m은 임의로 바꿉니다.
+            GUGU.n = 7;
+            GUGU.m = 3;
+
+            GUGU gugudan = new GUGU(9);
+        }
+    }
+}
