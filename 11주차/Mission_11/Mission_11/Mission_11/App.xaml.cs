c38c7476f8d4a8374b0780e58255dbe41f4be457
Judgement9882 <kjy6135@inu.ac.kr>
2022-05-17 22:24:57 +0900

    220517
    220517

---
+++ 11주차/Mission_11/Mission_11/Mission_11/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mission_11
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Mission_11.MainPage();
+
+#if DEBUG
+            TizenHotReloader.HotReloader.Open(this);
+#endif
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
