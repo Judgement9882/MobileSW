ad107d6028a4fc09b0ddb21f8921dbbca7bfd441
Judgement9882 <kjy6135@inu.ac.kr>
2022-05-18 17:55:27 +0900

    220518
    220518

---
+++ 12주차/MainPage.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+// 액자만들기
+namespace TizenXamlApp12
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var layout = new AbsoluteLayout();
+
+            var wall = new Image
+            {
+                Source = "wall.jpg"
+            };
+
+            var box1 = new BoxView
+            {
+                Color = Color.Black
+            };
+
+            var image1 = new Image
+            {
+                Source = "image1.jpg",
+                Aspect = Aspect.AspectFill
+            };
+
+            AbsoluteLayout.SetLayoutBounds(wall, new Rectangle(0, 0, 1920, 1080));
+            AbsoluteLayout.SetLayoutBounds(box1, new Rectangle(70, 70, 660, 660));
+            AbsoluteLayout.SetLayoutBounds(image1, new Rectangle(100, 100, 600, 600));
+
+            layout.Children.Add(wall);
+            layout.Children.Add(box1);
+            layout.Children.Add(image1);
+
+            Content = layout;
+        }
+    }
+}
+
+//// 이미지 사용법 (AbsoluteLayout)
+//namespace TizenXamlApp12
+//{
+//    [XamlCompilation(XamlCompilationOptions.Compile)]
+//    public partial class MainPage : ContentPage
+//    {
+//        public MainPage()
+//        {
+//            InitializeComponent();
+
+//            var layout = new AbsoluteLayout();
+
+//            var image1 = new Image
+//            {
+//                Source = "icon.png",
+//            };
+
+//            var image2 = new Image
+//            {
+//                Source = "icon.png",
+//            };
+
+//            var image3 = new Image
+//            {
+//                Source = "icon.png",
+//            };
+
+//            AbsoluteLayout.SetLayoutBounds(image1, new Rectangle(0, 0, 100, 100));
+//            AbsoluteLayout.SetLayoutBounds(image2, new Rectangle(860, 440, 200, 200));
+//            AbsoluteLayout.SetLayoutBounds(image3, new Rectangle(1620, 780, 300, 300));
+
+//            layout.Children.Add(image1);
+//            layout.Children.Add(image2);
+//            layout.Children.Add(image3);
+
+//            Content = layout;
+//        }
+//    }
+//}
+
+// 이미지 사용법 (StackLayout)
+//namespace TizenXamlApp12
+//{
+//    [XamlCompilation(XamlCompilationOptions.Compile)]
+//    public partial class MainPage : ContentPage
+//    {
+//        public MainPage()
+//        {
+//            InitializeComponent();
+
+//            var layout = new StackLayout();
+
+//            var image1 = new Image
+//            {
+//                Source = "icon.png",
+//                WidthRequest = 100,
+//                HeightRequest = 100,
+//                HorizontalOptions = LayoutOptions.StartAndExpand,
+//                VerticalOptions = LayoutOptions.StartAndExpand
+//            };
+
+//            var image2 = new Image
+//            {
+//                Source = "icon.png",
+//                WidthRequest = 100,
+//                HeightRequest = 100,
+//                HorizontalOptions = LayoutOptions.CenterAndExpand,
+//                VerticalOptions = LayoutOptions.CenterAndExpand
+//            };
+
+//            var image3 = new Image
+//            {
+//                Source = "icon.png",
+//                WidthRequest = 100,
+//                HeightRequest = 100,
+//                HorizontalOptions = LayoutOptions.EndAndExpand,
+//                VerticalOptions = LayoutOptions.EndAndExpand
+//            };
+
+//            layout.Children.Add(image1);
+//            layout.Children.Add(image2);
+//            layout.Children.Add(image3);
+
+//            Content = layout;
+//        }
+//    }
+//}
+
+
+// absolute layout 사용법
+//namespace TizenXamlApp12
+//{
+//    [XamlCompilation(XamlCompilationOptions.Compile)]
+//    public partial class MainPage : ContentPage
+//    {
+//        public MainPage()
+//        {
+//            InitializeComponent();
+
+//            var layout = new AbsoluteLayout();
+
+//            var label1 = new Label
+//            {
+//                Text = "Hello World!",
+//                TextColor = Color.Blue,
+//                FontSize = 100,
+//            };
+
+//            var label2 = new Label
+//            {
+//                Text = "Hello World!",
+//                TextColor = Color.Red,
+//                FontSize = 100,
+//            };
+
+//            var label3 = new Label
+//            {
+//                Text = "Hello World!",
+//                TextColor = Color.Yellow,
+//                FontSize = 100,
+//            };
+
+//            AbsoluteLayout.SetLayoutBounds(label1, new Rectangle(0, 0, 600, 200));
+//            AbsoluteLayout.SetLayoutBounds(label2, new Rectangle(700, 450, 600, 200));
+//            AbsoluteLayout.SetLayoutBounds(label3, new Rectangle(1320, 880, 600, 200));
+
+//            layout.Children.Add(label1);
+//            layout.Children.Add(label2);
+//            layout.Children.Add(label3);
+
+//            Content = layout;
+
+//        }
+//    }
+//}
---
+++ 12주차/Program.cs
@@ -0,0 +1,94 @@
+﻿// finally 포함 예제
+
+using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static int Divide(int divisor, int dividend)
+        {
+            try
+            {
+                Console.WriteLine("Divide() 시작");
+                return divisor / dividend;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Divide() 예외");
+                throw e;
+            }
+            finally
+            {
+                Console.WriteLine("Divide() 끝");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("제수를 입력하세요. :");
+                String temp = Console.ReadLine();
+                int divisor = Convert.ToInt32(temp);
+
+                Console.Write("피제수를 입력하세요. :");
+                temp = Console.ReadLine();
+                int dividend = Convert.ToInt32(temp);
+
+                Console.WriteLine("{0} / {1} = {2}", divisor, dividend, Divide(divisor, dividend));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("에러 : " + e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("zero 에러 : " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("프로그램을 종료합니다.");
+            }
+        }
+    }
+}
+
+// try catch, throw 예제
+
+//using System;
+
+//namespace ConsoleApp12
+//{
+//    class Program
+//    {
+//        static void DoSomething(int arg)
+//        {
+//            if (arg < 10)
+//            {
+//                Console.WriteLine($"arg : {arg}");
+//            }
+//            else
+//            {
+//                throw new Exception("arg가 10보다 큼");
+//            }
+//        }
+
+
+//        static void Main(string[] args)
+//        {
+//            int[] index = new int[6] { 1, 3, 5, 7, 9, 11 };
+//            try
+//            {
+//                foreach (int i in index)
+//                {
+//                    DoSomething(i);
+//                }
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine(e.Message);
+//            }
+//        }
+//    }
+//}
